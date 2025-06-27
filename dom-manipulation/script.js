dd44a5de49bb15012c5ad124ba5472d83293d1dc
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-06-27 17:10:01 +0100

    task 0

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,16 +1,47 @@
 
-const category  = [
-    "It is not life that matters, but the courage you bring into it.",
-    "Nigeria is not a nation. It is a mere geographical expression.",
-    "Violence never settles anything right. Apart from injuring your opponent, it also hurts you.",
-    "Any system of education which does not help a man to have a healthy and sound body, alert brain, and active mind is a false education.",
-    "The children of the poor you fail to train today will never let your children have peace.",
-    "I have never regarded myself as a tribalist; I am a nationalist of the highest order.",
-    "The black man shall be free, and in his freedom, he shall demonstrate the superiority of the mind over brute force.",
-    "The worst form of injustice is pretended justice.",
-    "Politics should be an avenue of service to the people and not an opportunity to make money.",
-    "The strong, the just, the selfless, the fearless, and the politically sagacious will always lead."
-]
+const AwoQuote = [
+    {
+        category: "Life",
+        text: "It is not life that matters, but the courage you bring into it."
+    },
+    {
+        category: "Nationhood",
+        text: "Nigeria is not a nation. It is a mere geographical expression."
+    },
+    {
+        category: "Peace",
+        text: "Violence never settles anything right. Apart from injuring your opponent, it also hurts you."
+    },
+    {
+        category: "Education",
+        text: "Any system of education which does not help a man to have a healthy and sound body, alert brain, and active mind is a false education."
+    },
+    {
+        category: "Social Justice",
+        text: "The children of the poor you fail to train today will never let your children have peace."
+    },
+    {
+        category: "Patriotism",
+        text: "I have never regarded myself as a tribalist; I am a nationalist of the highest order."
+    },
+    {
+        category: "Freedom",
+        text: "The black man shall be free, and in his freedom, he shall demonstrate the superiority of the mind over brute force."
+    },
+    {
+        category: "Justice",
+        text: "The worst form of injustice is pretended justice."
+    },
+    {
+        category: "Politics",
+        text: "Politics should be an avenue of service to the people and not an opportunity to make money."
+    },
+    {
+        category: "Leadership",
+        text: "The strong, the just, the selfless, the fearless, and the politically sagacious will always lead."
+    }
+];
+
 
 const quoteDisplay = document.getElementById('quoteDisplay')
 const newQuote = document.getElementById('newQuote')
@@ -18,7 +49,7 @@ const newQuote = document.getElementById('newQuote')
 
 function showRandomQuote(){
     const randomInt = Math.floor(Math.random() * 9) + 1;
-    quoteDisplay.innerHTML = `${category[randomInt]}`
+    quoteDisplay.innerHTML = `${AwoQuote[randomInt]["text"]}`
 }
 
 newQuote.addEventListener('click', showRandomQuote)
