5af14adcf48670aa3369a7e08f6e54a49cf0b660
adesina13 <dhikrullahabdullahadesina@gmail.com>
2025-06-27 19:39:44 +0100

    task 1

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,21 +1,21 @@
 
-
-
-const awo = localStorage.getItem('quote')
-let storedAwoQuote = JSON.parse(awo)
-
-
-
     const quoteDisplay = document.getElementById('quoteDisplay')
     const newQuote = document.getElementById('newQuote')
 
-
+    // Setting up the local Storage
+    const awo = localStorage.getItem('quote')
+    let storedAwoQuote = JSON.parse(awo)
+   
+    // Function that handules the randoom qoute
     function showRandomQuote(){
         if (storedAwoQuote !== null){
         const randomInt = Math.floor(Math.random() * (storedAwoQuote.length ));
         quoteDisplay.innerHTML = `<div>Category: ${storedAwoQuote[randomInt]["category"]}</div>
                                 <div>Text: ${storedAwoQuote[randomInt]["text"]}</div>`
-        
+        sessionStorage.setItem('recentQuote', JSON.stringify({
+            "category": storedAwoQuote[randomInt]["category"],
+            "text": storedAwoQuote[randomInt]["text"]
+        }))
         }else{
             alert("No Stored Quote")
         }
@@ -23,15 +23,24 @@ let storedAwoQuote = JSON.parse(awo)
 
     newQuote.addEventListener('click', showRandomQuote)
 
+    // SHow the last Showned Qoute
+    const recent = sessionStorage.getItem('recentQuote')
+    const recentQuote = JSON.parse(recent)
+
+    if (recentQuote !== null){
+        quoteDisplay.innerHTML = `<div>Category: ${recentQuote["category"]}</div>
+                                <div>Text: ${recentQuote["text"]}</div>`
+    }
 
 
-
+// Make the user to be able to add a new quote
 const createAddQuoteForm = document.createElement('div')
 createAddQuoteForm.innerHTML = `<div>
     <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
     <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
     <button onclick="addQuote()">Add Quote</button>
-</div>`
+</div>
+<input type="file" id="importFile" accept=".json" onchange="importFromJsonFile(event)" />`
 
 document.querySelector('body').appendChild(createAddQuoteForm)
 
@@ -60,4 +69,15 @@ function addQuote(){
     }else{
         alert("Fill the fields")
     }
+}
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+        const importedQuotes = JSON.parse(event.target.result);
+        quotes.push(...importedQuotes);
+        saveQuotes();
+        alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
 }
